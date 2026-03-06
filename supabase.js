2f38a6e186448a1333d6cbc435d61798cd692f33
Dduwcshuyzz <nkla0309.zz@gmail.com>
2026-03-07 01:25:51 +0700

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,10 +1,5 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
 
-<script>
-
-const SUPABASE_URL = "https://odresloilwzquaitspsuq.supabase.co";
-const SUPABASE_KEY = "sb_publishable_I1qFcqffCOJj49RtsPK82Q_98LcA2-8";
+const SUPABASE_URL = "https://odresloiiwzquaitspuq.supabase.co";
+const SUPABASE_KEY = "sb_publishable_I1qFcqffCOJj49RtsPK82Q_98LcA2-8";   // key của bạn
 
 window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-</script>
